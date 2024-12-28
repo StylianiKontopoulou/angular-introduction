b4ffdb06f4fce096d7fe69121efae6e3b401c0e3
Stella Kontopoulou <kontopouloustella@yahoo.ie>
2024-12-29 01:10:16 +0200

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { EventBindingExampleComponent } from './components/event-binding-example/event-binding-example.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'event-bind-example', component: EventBindingExampleComponent },
+    { path: 'welcome', component: WelcomeComponent },
+    { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  ];
