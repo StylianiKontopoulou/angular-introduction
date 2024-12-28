a56128f9ceacc50ef172d2f2e64d037353b4b2ea
Stella Kontopoulou <kontopouloustella@yahoo.ie>
2024-12-28 17:44:55 +0200

    component-input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
+import { Person } from './shared/interfaces/person';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,20 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 })
 export class AppComponent {
   name: 'Stella';
+
+  person0: Person = {
+    givenName: 'Stella',
+    surName: 'Kontopoulou',
+    age: 23,
+    email: 'stellakontopoulou@mail.com',
+    address: 'Athens, Greece'
+  };
+
+  person1: Person= {
+    givenName: 'John',
+    surName: 'Doe',
+    age: 0x25,
+    email: 'john@mail.com',
+    address: 'New York, USA'
+  };
 }
