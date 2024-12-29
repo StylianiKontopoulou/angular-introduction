ab8c313f800d2d6d857dc0567ff3c39fb3d6e07d
Stella Kontopoulou <kontopouloustella@yahoo.ie>
2024-12-29 16:44:14 +0200

    simple datatable

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,23 @@
 import { Routes } from '@angular/router';
-import { WelcomeComponent } from './components/welcome/welcome.component';
 import { EventBindingExampleComponent } from './components/event-binding-example/event-binding-example.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 import { ComponentInputExampleComponent } from './components/component-input-example/component-input-example.component';
 import { ForDirectiveExampleComponent } from './components/for-directive-example/for-directive-example.component';
+import { SimpleDatatableExampleComponent } from './components/simple-datatable-example/simple-datatable-example.component';
 
 export const routes: Routes = [
-    {path: 'component-input-example', component: ComponentInputExampleComponent},
-    { path: 'event-bind-example', component: EventBindingExampleComponent },
-    { path: 'for-directive-example', component: ForDirectiveExampleComponent },
-    { path: 'welcome', component: WelcomeComponent },
-    { path: '', redirectTo: '/welcome', pathMatch: 'full' },
-  ];
+  {
+    path: 'component-input-example',
+    component: ComponentInputExampleComponent,
+  },
+  {
+    path: 'for-directive-example',
+    component: ForDirectiveExampleComponent,
+  },
+  { path: 'event-binding-example', component: EventBindingExampleComponent },
+  {
+    path: 'simple-datatable-example',
+    component: SimpleDatatableExampleComponent,
+  },
+  { path: '', component: WelcomeComponent },
+];
--- src/app/components/list-group-menu/list-group-menu.component.ts
+++ src/app/components/list-group-menu/list-group-menu.component.ts
@@ -7,12 +7,16 @@ import { MenuItem } from 'src/app/shared/interfaces/menu-item';
   standalone: true,
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './list-group-menu.component.html',
-  styleUrl: './list-group-menu.component.css'
+  styleUrl: './list-group-menu.component.css',
 })
 export class ListGroupMenuComponent {
   menu: MenuItem[] = [
     { text: 'Component Input Example', routerLink: 'component-input-example' },
     { text: '@for Directive Example', routerLink: 'for-directive-example' },
     { text: 'Event Bind Example', routerLink: 'event-bind-example' },
+    {
+      text: 'Simple Datatable Example',
+      routerLink: 'simple-datatable-example',
+    },
   ];
 }
