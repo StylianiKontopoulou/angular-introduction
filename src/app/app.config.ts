bd7023c77066f120c15562e85aafbe0714688d84
Stella Kontopoulou <kontopouloustella@yahoo.ie>
2025-01-02 22:35:18 +0200

    fixed material styling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,6 +13,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptorsFromDi()),
+    provideHttpClient(withInterceptorsFromDi()), provideAnimationsAsync(),
   ],
 };
